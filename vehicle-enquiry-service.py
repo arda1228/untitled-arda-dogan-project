29d9f84b66d6cefcb7444dfc29ff57455866ba88
arda1228 <92720325+arda1228@users.noreply.github.com>
2023-05-11 22:08:51 +0100

    VES works

--- vehicle-enquiry-service.py
+++ vehicle-enquiry-service.py
@@ -4,22 +4,18 @@ import requests
 
 load_dotenv()
 
-key = os.getenv('VES_KEY_TEST')
-url = "https://uat.driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles"
+key = os.getenv('VES_KEY_UAT')
+prod_url = "https://driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles"
 
-# real url below
-# https://driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles
+uat_url = "https://uat.driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles"
 
-# test url below
-# https://uat.driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles
-
-payload = "{\n\t\"registrationNumber\": \"RJ04ULE\"\n}"
+payload = "{\n\t\"registrationNumber\": \"AA19AAA\"\n}"
 headers = {
   'x-api-key': key,
   'Content-Type': 'application/json'
 }
 
-response = requests.request("POST", url, headers=headers, data = payload)
+response = requests.request("POST", uat_url, headers=headers, data = payload)
 
 print(response.text.encode('utf8'))
 print(response.status_code)
