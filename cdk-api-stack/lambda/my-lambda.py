30f4d899d7bc3032e609eb9a2376eb7ad385cddf
arda1228 <92720325+arda1228@users.noreply.github.com>
2023-08-10 12:12:16 +0100

    used console to set up a layer that uses requests in the lambda to hit an external api - not documentation sorry

--- cdk-api-stack/lambda/my-lambda.py
+++ cdk-api-stack/lambda/my-lambda.py
@@ -3,7 +3,7 @@
 
 import json
 import os
-# import co2
+import requests
 
 def handler(event, context):
     print('request: {}'.format(json.dumps(event)))
@@ -13,19 +13,79 @@ def handler(event, context):
     destination = json.loads(event['body'])['destination']
 
     message = f'hello! my car\'s registration is {carReg}, and i want to know how much money and emissions i\'d save if i used public transport to get from {startingPoint} to {destination} instead of driving!\n\n'
-    # {co2.diff(starting_location=startingPoint, destination=destination)}'
     
     # Get the URL of the resource making the request
     origin_url = event.get('headers', {}).get('origin', '')
-    
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Content-Type': 'text/plain',
-            # "Access-Control-Allow-Origin": os.environ['FRONTEND_URL'],
-            "Access-Control-Allow-Origin": '*',
-            "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-            "Access-Control-Allow-Headers": "Content-Type, Authorization"
-        },
-        'body': f"{message}\n\nRequest came from: {origin_url}"
+
+
+
+    co2_st_url = os.environ['CO2_ST_URL']
+    co2_st_api_key = os.environ['CO2_ST_API_KEY']
+
+
+    # finding driing emissions
+    driving_data = {
+        "from": startingPoint,
+        "to": destination,
+        "transport_types": [
+            "driving"
+        ]
     }
+
+    headers = {
+        "Authorization": f"Bearer {co2_st_api_key}"
+    }
+
+    try:
+        driving_response = requests.post(co2_st_url, json=driving_data, headers=headers)
+
+        if driving_response.status_code == 200:
+            driving_result = driving_response.json()
+            driving_emissions = driving_result['trips'][0]['co2e']
+
+            # finding pt emissions
+            pt_data = {
+                "from": startingPoint,
+                "to": destination,
+                "transport_types": [
+                    "public-transport"
+                ]
+            }
+
+            try:
+                pt_response = requests.post(co2_st_url, json=pt_data, headers=headers)
+
+                if pt_response.status_code == 200:
+                    pt_result = pt_response.json()
+                    pt_emissions = pt_result['trips'][0]['co2e']
+
+                    # comparing them
+
+                    comparison_msg = f'you would save {driving_emissions - pt_emissions} kgs of carbon dioxide!'
+                    return {
+                    'statusCode': 200,
+                    'headers': {
+                        'Content-Type': 'text/plain',
+                        "Access-Control-Allow-Origin": os.environ['FRONTEND_URL'],
+                        # "Access-Control-Allow-Origin": '*',
+                        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+                        "Access-Control-Allow-Headers": "Content-Type, Authorization"
+                    },
+                    'body': f"{message}\n\n{comparison_msg}\n\nRequest came from:{origin_url}\n\n"
+                }
+
+                else:
+                    print(f"POST request failed with status code: {driving_response.status_code}")
+                    return None
+
+            except requests.exceptions.RequestException as e:
+                print(f"An error occurred during the POST request: {e}")
+                return None
+
+        else:
+            print(f"POST request failed with status code: {driving_response.status_code}")
+            return None
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred during the POST request: {e}")
+        return None
