4b45e5e633a1f5f38df26722f7a2a66c5d533190
arda1228 <92720325+arda1228@users.noreply.github.com>
2024-02-25 17:58:41 +0000

    DISTANCE

--- cdk-api-stack/lambda/my-lambda.py
+++ cdk-api-stack/lambda/my-lambda.py
@@ -4,6 +4,8 @@
 import json
 import os
 import requests
+# from geopy.distance import geodesic
+import numpy as np
 
 def calculate_journey_cost(distance, fuel_efficiency, fuel_price_per_liter):
     fuel_needed = distance / fuel_efficiency
@@ -149,9 +151,87 @@ def handler(event, context):
             driving_price = fuel_needed * fuel_prices[fuelType]
             driving_price += insurances[carSize][fuelType]
 
-            msg = f'by using public transport instead of driving, you saved {difference} kgs of co2 on your {driving_distance}km trip!. driving would have cost you £{round(driving_price,2)}!'
-            # msg = f'by using public transport instead of driving, you saved {difference} kgs of co2 on your {driving_distance}km trip!'
-            # msg = f'by using public transport instead of driving, you saved {difference} kgs of co2!'
+            msg = f'By using public transport instead of driving, you saved {difference} kgs of co2 on your {driving_distance}km trip, User! Driving would have cost you £{round(driving_price,2)}!'
+            
+            # fetch fuel prices from sainsbury's api
+            try:
+                sainos_response = requests.get('https://api.sainsburys.co.uk/v1/exports/latest/fuel_prices_data.json', verify=False)
+                if sainos_response.status_code == 200:
+                    msg += sainos_response.json().get('last_updated')
+                else:
+                    msg += f"Failed to fetch stations data. Status code: {sainos_response.status_code}"
+            except Exception as e:
+                msg += f"Error fetching stations data: {e}"
+
+            # geocode the starting postcode
+            try:
+                geocode_response_start = requests.get(
+                    f"https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?singleLine={startingPoint}&f=json&maxLocations=1",
+                    verify=False  # Disable SSL certificate verification for this request
+                )
+                if geocode_response_start.status_code == 200:
+                    data = geocode_response_start.json()
+                    if 'candidates' in data and data['candidates']:
+                        location = data['candidates'][0]['location']
+                        lat1, lon1 = location['y'], location['x']
+                        msg += f"Coordinates for {startingPoint}: {lon1}, {lat1}"
+                    else:
+                        lon1, lat1 = None
+                        msg += f"Could not find coordinates for postcode {startingPoint}"
+                else:
+                    msg += f"Failed to fetch coordinates. Status code: {geocode_response_start.status_code}"
+            except Exception as e:
+                msg += f"Error fetching coordinates: {e}"
+            
+            # geocode the destination postcode
+            try:
+                geocode_response_dest = requests.get(
+                    f"https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?singleLine={destination}&f=json&maxLocations=1",
+                    verify=False  # Disable SSL certificate verification for this request
+                )
+                if geocode_response_dest.status_code == 200:
+                    data = geocode_response_dest.json()
+                    if 'candidates' in data and data['candidates']:
+                        location = data['candidates'][0]['location']
+                        lat2, lon2 = location['y'], location['x']
+                        msg += f"Coordinates for {destination}: {lon2}, {lat2}"
+                    else:
+                        lon2, lat2 = None
+                        msg += f"Could not find coordinates for postcode {destination}"
+                else:
+                    msg += f"Failed to fetch coordinates. Status code: {geocode_response_dest.status_code}"
+            except Exception as e:
+                msg += f"Error fetching coordinates: {e}"
+
+            # measure the distance to the nearest fuel station
+            # try:
+            #     closest_station = None
+            #     min_distance = float('inf')
+
+            #     for station in sainos_response['stations']:
+            #         station_coordinates = (station['location']['latitude'], station['location']['longitude'])
+            #         distance = geodesic(coordinates, station_coordinates).kilometers
+            #         if distance < min_distance:
+            #             closest_station = station
+            #             min_distance = distance
+
+            #     msg += f"closest station is {closest_station}"
+            # except Exception as e:
+            #     msg += f"Error processing JSON data: {e}"
+                
+
+            # using haversine formula to calculate distance between starting point and destination
+
+            # Twyford, Loughborough(-0.86766,51.47797,-1.24361,52.76437)
+            R = 6371.0  # Earth radius in kilometers
+            lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
+            dlon = lon2 - lon1
+            dlat = lat2 - lat1
+            a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
+            c = 2 * np.arcsin(np.sqrt(a))
+            distance = R * c
+
+            msg += f"correct distance between {startingPoint} and {destination} is {distance} km"
 
             return {
                     'statusCode': 200,
